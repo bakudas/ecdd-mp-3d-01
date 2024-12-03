5e5afa206d6ee3b151c4ec3a8e40e76fa95cba66
Wilson <bakudas@gmail.com>
2024-12-03 15:16:10 -0300

    Implementação Fila Chat otimização

--- Assets/Scripts/ChatManager.cs
+++ Assets/Scripts/ChatManager.cs
@@ -1,43 +1,63 @@
-using Photon.Pun;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
+using Photon.Pun;
 using TMPro;
 using UnityEngine;
 
-public class ChatManager : MonoBehaviour
+public class Chat : MonoBehaviour
 {
-    [SerializeField] private TMP_InputField _textoMensagem;
-    [SerializeField] private GameObject _conteudo;
-    [SerializeField] private GameObject _mensagem;
+    [SerializeField] private TMP_InputField inputText;
+    [SerializeField] private GameObject content;
+    [SerializeField] private GameObject message;
     private PhotonView _photonView;
-    
 
-    // Start is called before the first frame update
-    void Start()
+    public Queue<GameObject> _filaMensagem = new Queue<GameObject>();
+    private int _chatMensagemQtdMax = 5;
+
+    private void Start()
     {
-        _photonView = GetComponent<PhotonView>();        
+        _photonView = GetComponent<PhotonView>();
     }
+
+
+    public GameObject CriaMensagem(string texto)
+    {
+        GameObject mensagem = Instantiate(message, content.transform);
+        mensagem.GetComponent<TMP_Text>().text = texto;
+        mensagem.GetComponent<RectTransform>().SetAsFirstSibling();
+        return mensagem;
+    }
+
 
     public void EnviaMensagem()
     {
         _photonView.RPC(
             "RecebeMensagem",
             RpcTarget.All,
-            PhotonNetwork.LocalPlayer.NickName + ": " + _textoMensagem.text
-        );
+            PhotonNetwork.LocalPlayer.NickName + ": " + inputText.text
+            );
 
-        _textoMensagem.text = "";
+        inputText.text = "";
+
     }
 
     [PunRPC]
     public void RecebeMensagem(string mensagemRecebida)
     {
-        GameObject mensagem = Instantiate(_mensagem, _conteudo.transform);
+        if (_filaMensagem.Count <= _chatMensagemQtdMax)
+        {
+            var mensagem = CriaMensagem(mensagemRecebida);
+            _filaMensagem.Enqueue(mensagem);
+        }
+        else
+        {
+            var tempMsg = _filaMensagem.Dequeue();
+            Destroy(tempMsg);
+            var mensagem = CriaMensagem(mensagemRecebida);
+            _filaMensagem.Enqueue(mensagem);
+        }
 
-        mensagem.GetComponent<TMP_Text>().text = mensagemRecebida;
-
-        // ultimas mensagens virão primeiro
-        mensagem.GetComponent<RectTransform>().SetAsFirstSibling();
     }
-
 }
